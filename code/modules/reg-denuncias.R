3521e55efbc6400c347d3253f85e4ebccde4b871
bmfazio <boris.fazio@outlook.com>
2018-11-19 11:31:12 -0500

    yeeaa

--- code/modules/reg-denuncias.R
+++ code/modules/reg-denuncias.R
@@ -1,6 +1,13 @@
 ## Registro Denuncias
 # recuerda: edad, sexo, ubicacion victima
-cap300 <- import("reg-nac-denuncias-faltas/611-Modulo1313/Capítulo_300_Denuncia_de_Delitos_2017.sav" %>% paste0(ineidir,.), setclass = "data.table")
+library(data.table)
+library(rio)
+library(dplyr)
+setwd("D:/datasets/inei")
+cap300 <- import("renadef/611-Modulo1313/Capítulo_300_Denuncia_de_Delitos_2017.sav",
+                 setclass = "data.table")
+cap200 <- import("renadef/611-Modulo1312/Capítulo_200_Denuncia_de_Delitos_2017.sav",
+                 setclass = "data.table")
 # Tasa de homicidio doloso
 homicidio <- c(1, 3, 4, 6, 7, 14)
                #  Homicidio simple Homicidio calificado (asesinato)   Homicidio por emoción violenta 
@@ -21,3 +28,28 @@ reg.violenciad <- cap300[is.element(IH208, c(feminicidio, tentativa, lesiones)) 
 
 reg.todopareja <- cap300[IVH305 >= 10 & IVH305 <= 19 & is.element(IVH310,c(1,3)),
                          .(lugar=NOMBREDD, sexo=IVH303, edad=IVH305)]
+
+
+########
+cap200$IH208_GENERICO %>% table
+cap200[IH208_GENERICO == "DELITOS CONTRA LA HUMANIDAD",24:26]$IH208_ESPECIFICO %>% table
+cap200[IH208_GENERICO == "DELITOS CONTRA LA LIBERTAD",24:26]$IH208_ESPECIFICO %>% table # incluye violacion y trafico de personas (libertad sexual/personal)
+cap200[IH208_GENERICO == "DELITOS CONTRA LA FAMILIA",24:26]$IH208_ESPECIFICO %>% table # nada relevante
+cap200[IH208_GENERICO == "DELITOS CONTRA LA VIDA, EL CUERPO Y LA SALUD",24:26]$IH208_ESPECIFICO %>% table
+
+especificos <- c("VIOLACION DE LA LIBERTAD PERSONAL",
+"VIOLACION DE LA LIBERTAD SEXUAL",
+"VIOLACION DE LA INTIMIDAD",
+"DESAPARICION FORZOSA",
+"HECHOS SEGUIDOS DE MUERTE",
+"HOMICIDIO",
+"HOMICIDIO CULPOSO",
+"INSTIGACION Y/O AYUDA AL SUICIDIO",
+"LESIONES",
+"TENTATIVA DE HOMICIDIO")
+
+cap200[is.element(IH208_ESPECIFICO, especificos),24:26]$IH208_MODALIDAD %>% table
+
+cap200[is.element(IH208_ESPECIFICO, especificos),24:26]$IH208_MODALIDAD %>% length
+
+cap200[IH211_1==1|IH211_8==1|IH211_13==1|IH211_15==1|IH211_16==1|IH211_17==1|IH211_18==1] %>% dim
