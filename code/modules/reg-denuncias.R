1e6ab3d278c8f00d17277026393f3f68f2173910
bmfazio <boris.fazio@outlook.com>
2018-11-21 19:57:30 -0500

    yep

--- code/modules/reg-denuncias.R
+++ code/modules/reg-denuncias.R
@@ -8,6 +8,8 @@ cap300 <- import("renadef/611-Modulo1313/Capítulo_300_Denuncia_de_Delitos_2017.
                  setclass = "data.table")
 cap200 <- import("renadef/611-Modulo1312/Capítulo_200_Denuncia_de_Delitos_2017.sav",
                  setclass = "data.table")
+cap100 <- import("renadef/611-Modulo1311/Capítulo_100_Denuncia_de_Delitos_2017.sav",
+                 setclass = "data.table")
 # Tasa de homicidio doloso
 homicidio <- c(1, 3, 4, 6, 7, 14)
                #  Homicidio simple Homicidio calificado (asesinato)   Homicidio por emoción violenta 
@@ -53,3 +55,13 @@ cap200[is.element(IH208_ESPECIFICO, especificos),24:26]$IH208_MODALIDAD %>% tabl
 cap200[is.element(IH208_ESPECIFICO, especificos),24:26]$IH208_MODALIDAD %>% length
 
 cap200[IH211_1==1|IH211_8==1|IH211_13==1|IH211_15==1|IH211_16==1|IH211_17==1|IH211_18==1] %>% dim
+
+
+
+##############
+library(stringi)
+violcol <- apply(cap200, 2, function(x) grepl("violencia", tolower(x)))
+
+whichvi <- apply(violcol, 2, sum)
+
+cap200[violcol[,"IH208_MODALIDAD"],"IH208_MODALIDAD"]$IH208_MODALIDAD %>% table
